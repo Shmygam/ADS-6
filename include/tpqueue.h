184570ee5555f7edf91fcdceab0f095b0e268312
Шмыгленко Никита <99179542+Shmygam@users.noreply.github.com>
2022-04-17 19:52:02 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,8 +11,8 @@ class TPQueue {
  public:
     TPQueue() :first(0), last(0) { }
     void push(T x) {
-        int i = last++ ;
-        for (i;(arr[i % size].prior < x.prior) && (--i >= first);) {
+        int i = last++;
+        for (i;(--i >= first) && (arr[i % size].prior < x.prior);) {
             arr[(i+1) % size] = arr[i % size];
         }
         arr[(i +1) % size] = x;
